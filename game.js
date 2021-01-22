9160c1889df89bee01311009eb13c08d737140ac
Leah O'Gorman <leah.p.ogorman@gmail.com>
2021-01-22 17:12:04 -0500

    adjustments to font

--- game.js
+++ game.js
@@ -1,176 +1,230 @@
 const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
+const answers = Array.from(document.getElementsByClassName('answerBtn'));
 const progressText = document.getElementById('progressText');
 const progressBarFull = document.getElementById('progressBarFull');
-const scoreText = document.getElementById('score');
+const scoreCount = document.getElementById('score');
 
 
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
+let questionsLeft = [];
 
 
 let questions = [
     {
         question: "Whose house do the girls live in?",
-        answers: {
-            a: "Dorothy's",
-            b: "Rose's",
-            c: "Blanche's",
-            d: "Coco's"
-        },
-        correctAnswer: "c"
+        A: "Dorothy's",
+        B: "Rose's",
+        C: "Blanche's",
+        D: "Coco's",
+        correctAnswer: "C"
     },
     {
         question: "What does Charmaine do that angers Blanche?",
-        answers: {
-            a: "Writes a novel using Blanche's sex life for the plot",
-            b: "Disinvites her from their father's funeral",
-            c: "She calls Blanche selfish",
-            d: "She auditioned for the same part in a play"
-        },
-        correctAnswer: "a"
+        A: "Writes a novel using Blanche's sex life for the plot",
+        B: "Disinvites her from their father's funeral",
+        C: "Calls Blanche selfish",
+        D: "Auditions for the same part in a play",
+        correctAnswer: "A"
     },
     {
         question: "How many kids does Dorothy have?",
-        answers: {
-            a: "Five",
-            b: "Five and her husband has 1 from an affair",
-            c: "Two",
-            d: "Zero"
-        },
-        correctAnswer: "c"
+        A: "Five",
+        B: "Five and her husband has 1 from an affair",
+        C: "Two",
+        D: "Zero",
+        correctAnswer: "C"
     },
     {
         question: "How do the girls end up living together?",
-        answers: {
-            a: "They are related",
-            b: "Dorothy and Rose answer an ad placed by Blanche in the newspaper",
-            c: "They share a house in a retirement community",
-            d: "They get a place together after all their houses burn down"
-        },
-        correctAnswer: "b"
+        A: "They are related",
+        B: "Dorothy and Rose answer an ad placed by Blanche in the newspaper",
+        C: "They share a house in a retirement community",
+        D: "They get a place together after all their houses burn down",
+        correctAnswer: "B"
     },
     {
         question: "Which actor chose to leave the show?",
-        answers: {
-            a: "Betty White",
-            b: "Rue McClanahan",
-            c: "Bea Miller",
-            d: "Estelle Getty"
-        },
-        correctAnswer: "c"
+        A: "Betty White",
+        B: "Rue McClanahan",
+        C: "Bea Arthur",
+        D: "Estelle Getty",
+        correctAnswer: "C"
     },
     {
         question: "Where is Dorothy from?",
-        answers: {
-            a: "St. Olaf Minnesota",
-            b: "The South",
-            c: "Queens",
-            d: "Brooklyn"
-        },
-        correctAnswer: "d"
+        A: "St. Olaf Minnesota",
+        B: "The South",
+        C: "Queens",
+        D: "Brooklyn",
+        correctAnswer: "D"
     },
     {
         question: "Who is Fifi?",
-        answers: {
-            a: "It's what Blanche calls her fur coat",
-            b: "An old friend of Sophia's",
-            c: "Rose's cat",
-            d: "A fake monkey made out of a traffic cone"
-        },
-        correctAnswer: "d"
+        A: "It's what Blanche calls her fur coat",
+        B: "An old friend of Sophia's",
+        C: "Rose's cat",
+        D: "A fake monkey made out of a traffic cone",
+        correctAnswer: "D"
     },
     {
         question: "What is the name of the retirement home Sophia was in?",
-        answers: {
-            a: "Twin Oaks",
-            b: "Shady Pines",
-            c: "Sunny Days",
-            d: "Shady Trees"
-        },
-        correctAnswer: "b"
+        A: "Twin Oaks",
+        B: "Shady Pines",
+        C: "Sunny Days",
+        D: "Shady Trees",
+        correctAnswer: "B"
     },
     {
         question: "How did Charlie die?",
-        answers: {
-            a: "Heart Attack during sex",
-            b: "In his sleep",
-            c: "He died after she told him to drop dead",
-            d: "We are never told"
-        },
-        correctAnswer: "a"
+        A: "Heart Attack during sex",
+        B: "In his sleep",
+        C: "He died after she told him to drop dead",
+        D: "We are never told",
+        correctAnswer: "A"
+    },
+    {
+        question: "Who does Sophia claim to have formerly been in business with ?",
+        A: "Tony Paglia",
+        B: "Mama Celeste",
+        C: "Ellio Betzios",
+        D: "Papa DiGiorno",
+        correctAnswer: "B"
+    },
+    {
+        question: "What dessert did the girls often eat?",
+        A: "Chocolate Chip Cookies",
+        B: "Baked Alaska",
+        C: "Lemon Bars",
+        D: "Cheesecake",
+        correctAnswer: "D"
+    },
+    {
+        question: "Who founded St. Olaf?",
+        A: "Heinrich von Andredunen",
+        B: "Gustav Lundqvist",
+        C: "Bernt Julius Muus",
+        D: "Gunter Lindstrom",
+        correctAnswer: "A"
+    },
+    {
+        question: "What was the founder of St. Olaf the first to do?",
+        A: "Invented fish flavoured ice cream",
+        B: "Was the first person to pickle a herring",
+        C: "Came up with the idea of canning salmon",
+        D: "Came up with the idea of canning tuna in it's own juices",
+        correctAnswer: "D"
+    },
+    {
+        question: "What did Rose study in university?",
+        A: "French",
+        B: "English",
+        C: "Pig Latin",
+        D: "Latin",
+        correctAnswer: "C"
+    },
+    {
+        question: "Where is Sophia from?",
+        A: "St. Olaf Minnesota",
+        B: "Sicily",
+        C: "Brooklyn",
+        D: "Florida",
+        correctAnswer: "B"
+    },
+    {
+        question: "Who has Sophia NOT claimed to have had a romantic relationship with?",
+        A: "Pablo Picasso",
+        B: "Charles de Gaulle",
+        C: "Charlie Chaplin",
+        D: "Winston Churchill",
+        correctAnswer: "C"
+    },
+    {
+        question: "What does Kate's husband do for work?",
+        A: "He's a heart surgeon",
+        B: "He's a pharmacist",
+        C: "He's a psychologist",
+        D: "He's a podiatrist",
+        correctAnswer: "D"
+    },
+    {
+        question: "What was the premise of the Golden Girls spin off?",
+        A: "The Girls running a bar",
+        B: "Blanche and a new group of tenants",
+        C: "Blanche, Sophia, and Rose running a hotel",
+        D: "Dorothy and Sophia back in New York",
+        correctAnswer: "C"
     },
     ];
 
-const CORRECT_BONUS = 10;
-const INCORRECT_DEFICIT = -10;
-const MAX_QUESTIONS =  10;
+const correctBonus = 10;
+const incorrectDeficit = -5;
+const totalQuestions =  11;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
+    questionsLeft = [...questions];
     getNewQuestion();
 }
 
 getNewQuestion = () => {
 
-    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    if(questionsLeft.length === 0 || questionCounter >= totalQuestions -1 ) {
         localStorage.setItem('mostRecentScore', score)
-        return window.location.assign('end.html')
+        return window.location.assign('thankyou.html')
     }
     questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/ ${MAX_QUESTIONS}`;
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    // progressText.innerText = `${questionCounter} of ${totalQuestions - 1}`;
+    progressBarFull.style.width = `${(questionCounter / totalQuestions) * 100}%`;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionIndex];
+    const questionIndex = Math.floor(Math.random() * questionsLeft.length)
+    currentQuestion = questionsLeft[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach( choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
+    answers.forEach( answer => {
+        const letter = answer.dataset['letter'];
+        answer.innerText = currentQuestion[letter];
     })
-    availableQuestions.splice(questionIndex, 1);
+    questionsLeft.splice(questionIndex, 1);
 
     acceptingAnswers = true;
 }
 
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
+answers.forEach(answer => {
+    answer.addEventListener("click", e => {
         if(!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
+        const selectedAnswer = selectedChoice.dataset['letter'];
 
         const classToApply =
-            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+            selectedAnswer == currentQuestion.correctAnswer ? "correct" : "incorrect";
         
         if(classToApply === 'correct') {
-            incrementScore(CORRECT_BONUS);
+            addScore(correctBonus);
         } else {
-            decrementScore(INCORRECT_DEFICIT);
+            minusScore(incorrectDeficit);
         }
 
-        selectedChoice.parentElement.classList.add(classToApply); 
+        selectedChoice.classList.add(classToApply); 
         setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply); 
+            selectedChoice.classList.remove(classToApply); 
             getNewQuestion();
         }, 1000);
     });
 });
 
-incrementScore = num => {
+addScore = num => {
     score +=num;
-    scoreText.innerText = score;
+    scoreCount.innerText = "Score: " + score;
 }
-decrementScore = num => {
+minusScore = num => {
     score +=num;
-    scoreText.innerText = score;
+    scoreCount.innerText = "Score: " + score;
 }
 
 startGame()
